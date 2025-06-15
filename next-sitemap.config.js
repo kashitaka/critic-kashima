d5886ef4500457cc3c0a23053c17720c0754070a
kashitaka <makkausu@gmail.com>
2025-06-15 20:03:27 +0900

    add more 5 posts

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://kashitaka.dev",
+  siteUrl: "https://critic-kashima.art",
   generateRobotsTxt: true,
   outDir: './docs',
 }
--- src/app/_components/inquiry.tsx
+++ src/app/_components/inquiry.tsx
@@ -6,7 +6,9 @@ export function InquiryButton() {
     <>
       <Link
         href={SITE_INQUIRY_URL}
-          className="block w-full md:w-auto px-6 py-3 text-base font-medium text-center text-gray-700 border border-gray-300 rounded hover:bg-gray-100"
+          className="block w-full md:w-auto px-6 py-3 text-base font-medium text-center border border-gray-300 rounded 
+          text-gray-700 dark:text-gray-200 
+          hover:bg-gray-100 dark:hover:bg-gray-900"
       >
         お問合せはこちら
       </Link>
